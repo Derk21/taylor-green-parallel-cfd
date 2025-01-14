89a55b09428e4ce31d39f967f39d4d8bedf7ace0
ffleisch <florian.fleischmann@campus.tu-berlin.de>
2025-01-14 15:10:39 +0100

    periodic grid coord initilization

---
+++ Makefile
@@ -0,0 +1,36 @@
+# Compiler settings
+CUDA_HOME ?= /usr/local/cuda
+NVCC = $(CUDA_HOME)/bin/nvcc
+CXX = clang
+CXXFLAGS = -O2 -std=c++17
+
+# Include directories
+INCLUDE_DIRS = -I$(CUDA_HOME)/include
+
+# Libraries
+LIBS = -L$(CUDA_HOME)/lib64 -lcudart -lcuda
+
+# Flags for compiling
+# NVCC_FLAGS = -arch=sm_80 -lineinfo -Xcompiler $(CXXFLAGS)
+NVCC_FLAGS = -lineinfo -Xcompiler $(CXXFLAGS)
+
+# Target to build the executable
+all: $(EXEC) main 
+
+# Rule for compiling the CUDA source file
+$(EXEC): $(SRC)
+	$(NVCC) $(NVCC_FLAGS) $(SRC) -o $(EXEC) $(LIBS)
+main: main.cu
+	$(NVCC) $(NVCC_FLAGS) main.cu -o main $(LIBS)
+
+
+# Clean target to remove object files and executable
+clean:
+	rm -f $(EXEC)
+
+# Run the program
+run: $(EXEC)
+	./$(EXEC)
+
+# Phony targets (not actual files)
+.PHONY: all clean run
---
+++ main.cu
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <cuda_runtime.h>
+#define N 128               // Grid size X
+#define M 128              // Grid size Y
+#define ITERATIONS 100000    // Number of iterations
+#define PERIODIC_START 0.0f
+#define PERIODIC_END 2 * M_PI
+
+#define CHECK_CUDA(call)                                               \
+    if ((call) != cudaSuccess)                                         \
+    {                                                                  \
+        std::cerr << "CUDA error at " << __LINE__ << ":" << std::endl; \
+        std::cerr << (cudaGetErrorString(call)) << std::endl;          \
+        exit(EXIT_FAILURE);                                            \
+    }
+
+
+
+void initializePeriodicGrid(float *periodic_grid, int n, int m)
+{
+    float dx = (PERIODIC_END - PERIODIC_START) / (n - 1);
+    float dy = (PERIODIC_END - PERIODIC_START) / (m - 1);
+    for (int y_i = 0; y_i < m; ++y_i)
+    {
+        for (int x_i = 1; x_i < n; ++x_i)
+        {
+            periodic_grid[y_i * n + x_i] = PERIODIC_START + y_i * dy; //y component 
+            periodic_grid[y_i * n + x_i - 1] = PERIODIC_START + x_i * dx; //x component 
+        }
+    }
+}
+
+
+int main()
+{
+    float *periodic_grid = (float *)malloc(N * M * 2 * sizeof(float));
+    //float *curr = (float *)malloc(N * M * 2 * sizeof(float));
+    //float *next = (float *)malloc(N * M * 2 * sizeof(float));
+
+
+    // Check for allocation failures
+    //if (curr == NULL || next == NULL)
+    if (periodic_grid == NULL)
+    {
+        std::cerr << "Memory allocation failed!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Initialize the grids
+    //initializePeriodicGrid(curr, N, M);
+    //initializePeriodicGrid(next, N, M);
+    initializePeriodicGrid(periodic_grid,N,M);
+
+    //float *d_curr;
+    //allocate memory on device    
+    //CHECK_CUDA(cudaMalloc(&d_curr, N * M * sizeof(float)));
+
+    //copy data to device
+    //CHECK_CUDA(cudaMemcpy(d_curr, curr, N * M * sizeof(float), cudaMemcpyHostToDevice));
+    std::cout << "first rows of periodic grid:" << std::endl;
+    for (int y_i = 0; y_i < 5; ++y_i)
+    {
+        for (int x_i = 1; x_i < 10; x_i+=2)
+        {
+            std::cout << periodic_grid[x_i-1] << "," << periodic_grid[x_i] <<" ";
+        }
+        std::cout << std::endl;
+    }
+    free(periodic_grid);
+}
