eda2676a7d538dfe2f6dce2d2b23a5c9b15be091
ffleisch <florian.fleischmann@campus.tu-berlin.de>
2025-02-09 16:24:22 +0100

    diffusion with more threads

--- src/diffuse.cu
+++ src/diffuse.cu
@@ -15,8 +15,6 @@ void diffuseExplicit(double *velocity_grid, double *velocity_grid_next, const in
 
 void diffuseExplicitStep(const double *velocity_grid, double *velocity_grid_next, double amount, const int n, const int m,const double dx)
 {
-    //double dx = (PERIODIC_END - PERIODIC_START) / (n - 1);
-    //double dy = (PERIODIC_END - PERIODIC_START) / (m - 1);
     for (int y_i = 0; y_i < m; y_i++)
     {
         for (int i = 0; i < n; i++)
@@ -54,8 +52,8 @@ void diffuseExplicit(double *velocity_grid, const int n, const int m,const doubl
     const double amount = DIFFUSIVITY * (TIMESTEP / SUBSTEPS_EXPLICIT);
     //TODO: add guard for breaking cfl condition
 
-    dim3 blockDim(TILE_SIZE,TILE_SIZE);
-    dim3 gridDim((n + TILE_SIZE-1)/TILE_SIZE,(n+ TILE_SIZE-1)/TILE_SIZE); 
+    dim3 blockDim(TILE_SIZE*2,TILE_SIZE);
+    dim3 gridDim(((2*n) + blockDim.x-1)/blockDim.x,(m+ blockDim.y-1)/blockDim.y); 
 
     for(int i = 0; i < SUBSTEPS_EXPLICIT; i++)
     {
@@ -69,36 +67,37 @@ __global__ void diffuseExplicitStep(double *velocity_grid,  const double amount,
 {
     int col = threadIdx.x + blockIdx.x * blockDim.x; 
     int row = threadIdx.y + blockIdx.y * blockDim.y; 
-    double tmp_u, tmp_v = 0.0;
-    int u_i = col * 2;
-    int v_i = (col * 2) + 1;
-    if (row < m && col < n)
+    double tmp_u = 0.0;
+    //int u_i = col * 2;
+    //int v_i = (col * 2) + 1;
+
+    if (row < m && col < 2*n)
     {
 
-        double u = velocity_grid[periodic_linear_Idx(u_i, row,2*n,m)];
-        double v = velocity_grid[periodic_linear_Idx(v_i, row)];
+        double u = velocity_grid[periodic_linear_Idx(col, row,2*n,m)];
+        //double v = velocity_grid[periodic_linear_Idx(v_i, row)];
 
-        double u_left = velocity_grid[periodic_linear_Idx(u_i - 2, row,2*n,m)];
-        double u_right = velocity_grid[periodic_linear_Idx(u_i + 2, row,2*n,m)];
-        double u_up = velocity_grid[periodic_linear_Idx(u_i, row + 1,2*n,m)];
-        double u_down = velocity_grid[periodic_linear_Idx(u_i, row - 1,2*n,m)];
+        double u_left = velocity_grid[periodic_linear_Idx(col - 2, row,2*n,m)];
+        double u_right = velocity_grid[periodic_linear_Idx(col + 2, row,2*n,m)];
+        double u_up = velocity_grid[periodic_linear_Idx(col, row + 1,2*n,m)];
+        double u_down = velocity_grid[periodic_linear_Idx(col, row - 1,2*n,m)];
 
-        double v_left = velocity_grid[periodic_linear_Idx(v_i - 2, row,2*n,m)];
-        double v_right = velocity_grid[periodic_linear_Idx(v_i + 2, row,2*n,m)];
-        double v_up = velocity_grid[periodic_linear_Idx(v_i, row + 1,2*n,m)];
-        double v_down = velocity_grid[periodic_linear_Idx(v_i, row - 1,2*n,m)];
+        //double v_left = velocity_grid[periodic_linear_Idx(v_i - 2, row,2*n,m)];
+        //double v_right = velocity_grid[periodic_linear_Idx(v_i + 2, row,2*n,m)];
+        //double v_up = velocity_grid[periodic_linear_Idx(v_i, row + 1,2*n,m)];
+        //double v_down = velocity_grid[periodic_linear_Idx(v_i, row - 1,2*n,m)];
 
         double u_diffusion = (u_right - 2 * u + u_left) / (dx * dx) + (u_up - 2 * u + u_down) / (dx * dx);
-        double v_diffusion = (v_right - 2 * v + v_left) / (dx * dx) + (v_up - 2 * v + v_down) / (dx * dx);
+        //double v_diffusion = (v_right - 2 * v + v_left) / (dx * dx) + (v_up - 2 * v + v_down) / (dx * dx);
 
         tmp_u = u + amount * u_diffusion;
-        tmp_v = v + amount * v_diffusion;
+        //tmp_v = v + amount * v_diffusion;
     }
     __syncthreads();
-    if (row < m && col < n)
+    if (row < m && col < 2*n)
     {
-        velocity_grid[periodic_linear_Idx(u_i, row,2*n,m)] = tmp_u;
-        velocity_grid[periodic_linear_Idx(v_i, row,2*n,m)] = tmp_v;
+        velocity_grid[periodic_linear_Idx(col, row,2*n,m)] = tmp_u;
+        //velocity_grid[periodic_linear_Idx(v_i, row,2*n,m)] = tmp_v;
     }
 }
 }
